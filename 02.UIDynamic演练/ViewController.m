1435542e68285f9abab5d8c56f690981f66c3092
apple <apple@teacher.local>
2013-12-24 09:40:04 +0800

    Initial Commit

---
+++ 02.UIDynamic演练/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  02.UIDynamic演练
+//
+//  Created by apple on 13-12-24.
+//  Copyright (c) 2013年 itcast. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
